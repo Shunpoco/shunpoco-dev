c1d0b54ad1455c72b52915d6893fa735303e52dc
Shunpoco <tkngsnsk313320@gmail.com>
2020-11-09 03:51:17 +0900

    use docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,20 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path');
+const fs = require('fs');
 
-// You can delete this file if you're not using it
+exports.onPreInit = () => {
+  if (process.argv[2] === 'build') {
+    fs.rmdirSync(path.join(__dirname, 'docs'), { recursive: true });
+    fs.renameSync(
+      path.join(__dirname, 'public'),
+      path.join(__dirname, 'public_dev'),
+    );
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'));
+  fs.renameSync(
+    path.join(__dirname, 'public_dev'),
+    path.join(__dirname, 'public'),
+  );
+}
