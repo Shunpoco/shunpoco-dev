52af8e988f8a63a54fe1842414d8b74c823baf36
Shunpoco <tkngsnsk313320@gmail.com>
2020-12-13 22:16:09 +0900

    move assets and blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/contents/assets`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,13 +24,13 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `contents/assets/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/markdowns`,
+        path: `${__dirname}/contents/blog`,
         name: `markdowns`,
       },
     },
