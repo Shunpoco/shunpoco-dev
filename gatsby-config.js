6dfd85865da508e4170b30e30855fd4f09d3e6a4
Shunpoco <tkngsnsk313320@gmail.com>
2020-12-13 23:48:47 +0900

    change manifest name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `shunpoco-dev`,
+        short_name: `shunpoco`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
